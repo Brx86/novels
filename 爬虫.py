be11c1c01b3724edd4927681037a4c83e20ac351
Brx86 <brx86@qq.com>
2023-03-15 15:33:28 +0800

    fix

--- 爬虫.py
+++ 爬虫.py
@@ -3,7 +3,6 @@ from lxml import etree
 
 
 def 获取文章列表():
-    全部文章列表 = []
     for 页码 in range(1, 4):
         链接 = f"http://futaningmengcha991.lofter.com/?page={页码}"
         请求结果 = httpx.get(链接)
@@ -12,18 +11,17 @@ def 获取文章列表():
         for 文章 in 每页文章列表:
             网址 = 文章.xpath("@href")[0]
             标题 = 文章.xpath("span/h2/text()")[0]
-            全部文章列表.append((标题, 网址))
-    return 全部文章列表
+            yield (标题, 网址)
 
 
 def 下载文章(标题, 链接):
     请求结果 = httpx.get(链接)
     网页内容 = etree.HTML(请求结果.text.replace("&nbsp;", " "), parser=None)
     文章内容 = 网页内容.xpath('//div[@class="text"]/p/text()')
-    with open(f"小说.md", "a") as f:
-        f.write(f"## {标题}\n\n")
+    with open(f"小说.md", "a") as 文件:
+        文件.write(f"## {标题}\n\n")
         for 段落 in 文章内容:
-            f.write(f"{段落}\n\n")
+            文件.write(f"{段落}\n\n")
 
 
 for 文章 in 获取文章列表():
